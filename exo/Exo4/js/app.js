05c670e62557a0693ec0adf60cd64a7a5f24b01e
TristanValk <154213073+TristanMPN7@users.noreply.github.com>
2023-12-20 17:46:18 +0100

    Add files via upload

---
+++ exo/Exo4/js/app.js
@@ -0,0 +1,36 @@
+//const button = document.getElementById("validation-button");
+
+//button.addEventListener("click", function () {
+//console.log("C'est cliqué !"); // Pour afficher un message dans la console navigateur.
+//window.alert("Hello !"); // Pour pouvoir afficher une fenêtre de navigation dans le navigateur.
+//});
+
+//Les variables doivent être déclarés avant d'être utilisées.
+//let number = 15;
+//console.log("number");
+
+//let string = "Une chaîne de caractère";
+//console.log("string");
+
+//let booleen = true;
+//console.log("number");
+
+//let sayhello = "hello " + pseudo;
+//let pseudo = "Tristan";
+
+let firstName = "Tristan";
+console.log(firstName);
+
+let lasteName = "Martinez";
+console.log(lasteName);
+
+let number1 = 60;
+console.log(number1);
+
+let number2 = 100 + number1;
+console.log(number2);
+
+let calculate = number2 * 5 - 40;
+console.log(calculate);
+
+window.prompt("Quel est la valeur HT ?");
